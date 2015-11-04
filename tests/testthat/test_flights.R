ad58821fa47608d819717a25ab49d63d893225d0
Philippe Chataignon <philippe@main>
2015-11-04 11:17:18 +0100

    Add tests directory

---
+++ tests/testthat/test_flights.R
@@ -0,0 +1,16 @@
+library(nycflights13)
+s <- src_netezza('NZN')
+copy_to(s, flights, replace=T)
+
+fl <- tbl(s, 'FLIGHTS')
+fl  %>% filter(origin %in% c('LGA', 'EWR')) %>% tally() %>% collect()  %>% as.numeric() -> eff2
+test_that("filter OK",	{
+    expect_equal(eff2, 225497)
+})
+
+appa <- data.frame(ok=c('EWR', 'LGA'), test=1)
+fl %>% semi_join(appa, by=c('origin' = 'ok'), copy=T) %>% tally()  %>% collect()  %>% as.numeric() -> eff1
+
+test_that("semi_join with copy OK",	{
+    expect_equal(eff1, 225497)
+})
