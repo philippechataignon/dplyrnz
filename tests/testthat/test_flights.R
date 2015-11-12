d0392ad43247d5d6bb670a7e009399cb950cc6d6
Philippe Chataignon <philippe@main>
2015-11-12 14:11:04 +0100

    Adapt test to temporary table

--- tests/testthat/test_flights.R
+++ tests/testthat/test_flights.R
@@ -1,16 +1,22 @@
 library(nycflights13)
 s <- src_netezza('NZN')
-#copy_nycflights13(s)
-copy_to(s, flights, name='FLIGHTS', temporary = F)
+copy_to(s, flights, name='FLIGHTS', temporary = T)
 
 fl <- tbl(s, 'FLIGHTS')
-fl  %>% filter(origin %in% c('LGA', 'EWR')) %>% tally() %>% collect()  %>% as.numeric() -> eff2
+fl  %>% filter(origin %in% c('LGA', 'EWR')) %>%
+    tally() %>%
+    collect() %>%
+    as.numeric() -> eff2
+
 test_that("filter OK",	{
     expect_equal(eff2, 225497)
 })
 
 appa <- data.frame(ok=c('EWR', 'LGA'), test=1)
-fl %>% semi_join(appa, by=c('origin' = 'ok'), copy=T) %>% tally()  %>% collect()  %>% as.numeric() -> eff1
+fl %>%
+    semi_join(appa, by=c('origin' = 'ok'), copy=T) %>%
+    tally()  %>% collect() %>%
+    as.numeric() -> eff1
 
 test_that("semi_join with copy OK",	{
     expect_equal(eff1, 225497)
